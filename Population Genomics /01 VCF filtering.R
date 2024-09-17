68da58393e3259ead4de6f64ed25ac72b2f488bd
Kaeli Elizabeth Gusek <kgusek@node300.cluster>
2024-09-17 17:10:30 -0400

    9/17/24

--- Population Genomics /01 VCF filtering.R
+++ Population Genomics /01 VCF filtering.R
@@ -20,3 +20,46 @@ plot(chr1)
 pdf(file="~/Projects/eco_genomics_2/Population Genomics /Figures/ChromoPlot.pdf")
 chromoqc(chr1, xlim=c(1e1, 1.1e8))
 dev.off()
+library(vcfR)
+setwd("/gpfs1/cl/pbio3990/PopulationGenomics/")
+list.files("variants/")
+vcf <- read.vcfR("variants/Centaurea_filtered.vcf.gz")
+vcf
+head(vcf)
+DP <- extract.gt(vcf, element="DP", as.numeric=T) #extract elements from vcf files 
+DP[1:5,1:10] #number of reads total for that chromosome in that position 
+dim(DP)
+quantile(DP)
+DP[DP==0] <- NA
+quantile(DP)
+quantile(DP, na.rm=T)
+#visualize the matrix of depth and missingness in the vcf file
+heatmap.bp(DP)
+heatmap.bp(DP[1:1000,], rlabels=F, clabels=F)
+library(SNPfiltR)
+hard_filter(vcf) #histogram of DP values 
+vcf.filt <- hard_filter(vcf, depth=3) 
+#if you dont have at least three reads at a given SNP position for an individual set it at NA
+#could explore values DP 5, 10...
+max_depth(vcf.filt) # max depth filter 
+vcf.file <- max_depth(vcf.filt, maxdepth=60)#filter out genotype with >60 reads/SNPs
+meta <- read.csv("metadata/meta4vcf.csv", header=T)
+meta2 <- meta[,c(1,4)]
+names(meta2) <- c("id","pop")
+meta2$id = as.factor(meta2$id)
+meta2$pop = as.factor(meta2$pop)
+vcf.filt.indMiss <- missing_by_sample(vcf.filt,
+                                      popmap = meta2,
+                                      cutoff=0.75)
+#filtering at 75%, drop individuals from 5/6 pops
+vcf.filt.indMiss <- filter_biallelic(vcf.filt.indMiss)
+vcf.filt.indMiss <- min_mac(vcf.filt.indMiss, min.mac = 1)
+vcf.filt.indSNPMiss <- missing_by_snp(vcf.filt.indMiss, cutoff=0.5)
+DP2 <- extract.gt(vcf.filt.indSNPMiss,
+                  element="DP",
+                  as.numeric=T)
+
+heatmap.bp(DP2[1:5000,],
+           rlabels=F, clabels=F)
+write.vcf(vcf.filt.indSNPMiss,
+          "~/Projects/eco_genomics_2/Population Genomics /Outputs/vcf_final.filtered.vcf.gz")
--- Population Genomics /popgen_notes.md
+++ Population Genomics /popgen_notes.md
@@ -41,3 +41,43 @@ plot(chr1): plots for the data
 chromoqc(chr1, xlim=c(1e1, 1.1e8)): plots from one telomere to the other, each dot is a SNP in the genome
 
 pdf: pdf writer
+
+### 9/17/24 Filtering Strategies 
+
+Filtered vcf files based on missingness and depth, cut off outliers
+
+0/0 reference homozygote
+
+0/1 Heterozygote
+
+./. N/A
+
+Low depth: limited accuracy on the nucleotide in that position/what the genotype call should be
+
+High depth: assembly error in the ref genome or gene paralogy (reads from duplicated genes mapping to the same position)
+
+Missingness: individual level or SNP level (depending on site, may be poorly represented)
+
+Low freq alleles: less than 1% in the population
+
+The white on the heat map is missing data
+
+row is individuals
+
+columns are sequence data
+
+df[ rows, column] leaving it blank will leave all of the rows and columns
+
+? in console bring up an index to give information on a function
+
+DP \<- extract.gt(vcf, element="DP", as.numeric=T): extract elements from vcf files DP[1:5,1:10]: number of reads total for that chromosome in that position
+
+quantile(DP, na.rm=T): visualize the matrix of depth and missingness in the vcf file
+
+Cutoff: set a cutoff value for individuals you do not want to include based on depth and missingness
+
+if you dont have at least three reads at a given SNP position for an individual set it at NA
+
+max_depth(vcf.filt): max depth filter
+
+write.vcf(vcf.filt.indSNPMiss, "\~/Projects/eco_genomics_2/Population Genomics /Outputs/vcf_final.filtered.vcf.gz"): save files, make sure to include file types (vcf.gz)
